2fc012d3f9f49a6096b0c7a0583f07b02193a23f
Fergal Riordan <riordanf@tcd.ie>
2023-03-20 18:40:43 +0000

    Observer implemented and IDE problems resolved (?)

---
+++ src/main/java/is/hi/hbv202g/ass9/compositeLeafObservedTemplateMethod/PlusComposite.java
@@ -0,0 +1,37 @@
+package is.hi.hbv202g.ass9.compositeLeafObservedTemplateMethod;
+
+import java.util.List;
+import java.util.ArrayList;
+
+public class PlusComposite implements MathExpression, Observer {
+    private List<MathExpression> children = new ArrayList<>();
+    private int lastObservedResult;
+
+    public int getResult() {
+        int result = 0;
+        for (MathExpression child : children) {
+            result += child.getResult();
+        }
+        return result;
+    }
+
+    public void add(MathExpression child) {
+        children.add(child);
+    }
+
+    public void remove(MathExpression child) {
+        children.remove(child);
+    }
+
+    public void update() {
+        this.lastObservedResult = 0;
+        for (MathExpression child : children) {
+            this.lastObservedResult += child.getResult();
+        }
+        System.out.println(this.lastObservedResult);
+    }
+
+    public int getLastObservedResult() {
+        return this.lastObservedResult;
+    }
+}
